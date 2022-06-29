7b4197485e8e025e1fa5e76c1cb3068770c22ac5
ryuryu0226 <ryuryu19990226@gmail.com>
2022-06-29 23:00:04 +0900

    サーバーから位置情報を取得してピンを表示

--- GeospatialSample/Assets/Scripts/SampleScript.cs
+++ GeospatialSample/Assets/Scripts/SampleScript.cs
@@ -21,72 +21,86 @@ public class SampleScript : MonoBehaviour
     public Text OutputText; // 結果表⽰⽤のUI
     public double HeadingThreshold = 25; // ⽅位の許容精度
     public double HorizontalThreshold = 20; // ⽔平位置の許容精度
-    public double Latitude; // オブジェクトを置く緯度
-    public double Longitude; // オブジェクトを置く緯度
-    public double Altitude; // オブジェクトを置く⾼さ
-    public double Heading; // オブジェクトの⽅位 (北=0°)
     public GameObject ContentPrefab; //表⽰オブジェクトの元データ
     GameObject displayObject; //実際に表⽰するオブジェクト
     public ARAnchorManager AnchorManager; //アンカー作成に使⽤
     public Text NetworkResultText; //ネットワーク通信の結果を表示
     float span = 3f;
     private float currentTime = 0f;
+    string host = "160.251.18.95";  // VPS サーバーに送信する場合 (グローバル通信)
+    // string host = "localhost";      // 同一マシン内のサーバーに送信する場合
+    // string host = "192.168.32.237";   // ローカルネットワーク内のサーバーに送信する場合 (プライベートアドレスを固定する必要アリ)
+    // string host = "0.0.0.0";
+    int port = 12345; //ネットワーク通信用ポート番号
+    public string myname;
+    public string friendname;
+    public Location friendLoc;
+    public static float mylat = 34.64611f;
+    public static float mylon = 135.0041f;
 
     void Start()
     {
-        Debug.Log("Start() has called.");
-        NetworkCallTest();
-        Debug.Log("NetworkCallTest() done.");
-        GameObject zero = Instantiate(ContentPrefab, Vector3.zero, Quaternion.identity);
+        // 原点（アプリケーションの起動位置）にピンを表示
+        // GameObject zero = Instantiate(ContentPrefab, Vector3.zero, Quaternion.identity);
     }
     // Update is called once per frame
     void Update()
     {
-        // currentTime += Time.deltaTime;
+        string status = "";
 
-        // if(currentTime > span){
-        //     Debug.LogFormat ("{0}秒経過", span);
-        //     currentTime = 0f;
-        //     NetworkCallTest();
-        // }
-        
-        // string status = "";
+        // 初期化失敗またはトラッキングができたいない場合は何もしないで戻る
+        if (!Initializer.IsReady || EarthManager.EarthTrackingState != TrackingState.Tracking)
+        {
+            return;
+        }
+        // トラッキング結果を取得
+        GeospatialPose pose = EarthManager.CameraGeospatialPose;
+        mylat = (float)pose.Latitude;
+        mylon = (float)pose.Longitude;
 
-        // // 初期化失敗またはトラッキングができたいない場合は何もしないで戻る
-        // if (!Initializer.IsReady || EarthManager.EarthTrackingState != TrackingState.Tracking)
-        // {
-        //     return;
-        // }
-        // // トラッキング結果を取得
-        // GeospatialPose pose = EarthManager.CameraGeospatialPose;
+        // 位置情報を送受信
+        currentTime += Time.deltaTime;
+        if(currentTime > span)
+        {
+            currentTime = 0f;
+            LocationSend(myname, pose.Latitude, pose.Longitude, pose.Altitude);
+            LocationGet(friendname);
+        }
 
-        // // トラッキング精度がthresholdより悪い(値が⼤きい)場合
-        // if (pose.HeadingAccuracy > HeadingThreshold || pose.HorizontalAccuracy > HorizontalThreshold)
-        // {
-        //     status = "低精度︓周辺を⾒回してください";
-        // }
-        // else // 許容誤差以内の場合
-        // {
-        //     status = "⾼精度︓High Tracking Accuracy";
+        // トラッキング精度がthresholdより悪い(値が⼤きい)場合
+        if (pose.HeadingAccuracy > HeadingThreshold || pose.HorizontalAccuracy > HorizontalThreshold)
+        {
+            status = "低精度︓周辺を⾒回してください";
+        }
+        else // 許容誤差以内の場合
+        {
+            status = "⾼精度︓High Tracking Accuracy";
 
-        //     // 初めて⾼精度計測できたタイミングでオブジェクトを⽣成
-        //     if (displayObject == null)
-        //     {
-        //         // Altitude = pose.Altitude - 1.5f; // スマホの⾼さ-1.5mでおよそ地⾯の⾼さとする (tentatively)
-        //         Altitude = pose.Altitude;
+            // サーバーから取得した緯度・経度・高度にピンを表示
+            ARGeospatialAnchor anchor = AnchorManager.AddAnchor(friendLoc.Latitude, friendLoc.Longitude, friendLoc.Altitude, Quaternion.identity); // 指定した位置・向きのアンカーを作成 (緯度・経度・⾼度をx,y,zに、⽅位を姿勢に変換)
 
-        //         Quaternion quaternion = Quaternion.AngleAxis(180f - (float)Heading, Vector3.up);  // ⾓度の補正(このあと使うAnchor⽣成関数が、南=0を前提としているため)
-        //         ARGeospatialAnchor anchor = AnchorManager.AddAnchor(Latitude, Longitude, Altitude, quaternion); // 指定した位置・向きのアンカーを作成 (緯度・経度・⾼度をx,y,zに、⽅位を姿勢に変換)
+            // 初めて⾼精度計測できたタイミングでオブジェクトを⽣成
+            if (displayObject == null)
+            {
+                // double Altitude = pose.Altitude; // スマホの⾼さ-1.5mでおよそ地⾯の⾼さとする (tentatively)
 
-        //         // アンカーが正しく作られていればオブジェクトを実体化
-        //         if (anchor != null)
-        //         {
-        //             displayObject = Instantiate(ContentPrefab, anchor.transform);
-        //         }
-        //     }
-        // }
-        // //結果を表⽰(statusはのちほど使う)
-        // ShowTrackingInfo(status, pose, displayObject);
+                // 自分のGPSで取得した位置から緯度・経度少し話した位置にピンを表示
+                // Quaternion quaternion = Quaternion.AngleAxis(180f - (float)pose.Heading, Vector3.up);  // ⾓度の補正(このあと使うAnchor⽣成関数が、南=0を前提としているため)
+                // ARGeospatialAnchor anchor = AnchorManager.AddAnchor(pose.Latitude + 0.0001, pose.Longitude + 0.0001, pose.Altitude, quaternion); // 指定した位置・向きのアンカーを作成 (緯度・経度・⾼度をx,y,zに、⽅位を姿勢に変換)
+
+                // アンカーが正しく作られていればオブジェクトを実体化
+                if (anchor != null)
+                {
+                    displayObject = Instantiate(ContentPrefab, anchor.transform);
+                }
+            }
+            else
+            {
+                displayObject.transform.position = anchor.transform.position;
+            }
+        }
+        //結果を表⽰(statusはのちほど使う)
+        ShowTrackingInfo(status, pose, displayObject);
         
 
     }
@@ -118,17 +132,61 @@ public class SampleScript : MonoBehaviour
         );
     }
 
+
+    public async void LocationSend(string Username, double Latitude, double Longitude, double Altitude)
+    {
+        NetworkResultText.text = "LocationSend() has called.";
+        Debug.Log("LocationSend() has called.");
+
+        // http用に変更
+        var channel = new Channel(host, port, ChannelCredentials.Insecure);
+        var client = MagicOnionClient.Create<IMyFirstService>(channel);
+
+        /* 位置情報-送信 */
+        // 自分のユーザー名と位置情報を送信する
+        Location my_location = new Location();  // 独自定義クラス Location のインスタンスを作成して情報を埋める (定義は /ARLocationSharing/NetworkApp-Shared/Server/Services/Interface/IMyFirstService.cs )
+        my_location.Username = Username;
+        my_location.Latitude = Latitude;
+        my_location.Longitude = Longitude;
+        my_location.Altitude = Altitude;
+        my_location.Exist = true;
+
+        bool res = await client.SendLocation(my_location); // サーバーへ送信 (SendLocation の実装は /ARLocationSharing/NetworkApp-Server/Services/MyFirstService.cs )
+        Debug.Log($"SendLocation Result: {res}");
+        NetworkResultText.text += $"\n SendLocation Result: {res}";
+    }
+
+
+    public async void LocationGet(string Username)
+    {
+        NetworkResultText.text = "LocationGet() has called.";
+        Debug.Log("LocationGet() has called.");
+
+        // http用に変更
+        var channel = new Channel(host, port, ChannelCredentials.Insecure);
+        var client = MagicOnionClient.Create<IMyFirstService>(channel);
+
+        /* 位置情報-受信 */
+        // 引数 username に該当するユーザーの位置情報を取得する
+        Location loc = await client.GetLocation(Username); // サーバーへ送信 (GetLocation の実装は /ARLocationSharing/NetworkApp-Server/Services/MyFirstService.cs )
+        if (loc.Exist) // 該当ユーザーが存在する
+        {
+            Debug.Log($"GetLocation Result: name={loc.Username} lat={loc.Latitude.ToString()} lon={loc.Longitude.ToString()}");
+            friendLoc = new Location();
+            friendLoc = loc;
+        }
+        else // 該当ユーザーが存在しない
+        {
+            Debug.Log($"GetLocation Result: name={Username} not registerd in server");
+        }
+        NetworkResultText.text += $"\n GetLocation Result: name={loc.Username}, lat={loc.Latitude}, lon={loc.Longitude}, alt={loc.Altitude}";
+    }
+
+
     public async void NetworkCallTest()
     {
         NetworkResultText.text = "NetworkCallTest() has called.";
         Debug.Log("NetworkCallTest() has called.");
-
-        string host = "160.251.18.95";  // VPS サーバーに送信する場合 (グローバル通信)
-        // string host = "localhost";      // 同一マシン内のサーバーに送信する場合
-        // string host = "192.168.32.237";   // ローカルネットワーク内のサーバーに送信する場合 (プライベートアドレスを固定する必要アリ)
-        // string host = "0.0.0.0";
-
-        int port = 12345; //ネットワーク通信用ポート番号
 
         // http用に変更
         var channel = new Channel(host, port, ChannelCredentials.Insecure);
@@ -142,8 +200,6 @@ public class SampleScript : MonoBehaviour
         my_location.Longitude = 135.455982008042;
         my_location.Altitude = 93;
         my_location.Exist = true;
-
-        NetworkResultText.text += string.Format($"\n {0}", my_location.ToString());
 
         bool res = await client.SendLocation(my_location); // サーバーへ送信 (SendLocation の実装は /ARLocationSharing/NetworkApp-Server/Services/MyFirstService.cs )
         Debug.Log($"SendLocation Result: {res}");
@@ -164,7 +220,5 @@ public class SampleScript : MonoBehaviour
             Debug.Log($"GetLocation Result: name={username} not registerd in server");
         }
         NetworkResultText.text += $"\n GetLocation Result: name={loc.Username}, lat={loc.Latitude}, lon={loc.Longitude}, alt={loc.Altitude}";
-
-
     }
 }
--- GeospatialSample/Assets/Scripts/map/DrawGoogleMap.cs
+++ GeospatialSample/Assets/Scripts/map/DrawGoogleMap.cs
@@ -5,28 +5,39 @@ using System;
 using UnityEngine.UI;
 
 public class DrawGoogleMap : MonoBehaviour {
-    public float lat = 34.64611f;
-    public float lng = 135.0041f;
     public string key = null;
     public int zoom = 15;
 
     // Google Maps Embed API
-    string Url = @"https://maps.googleapis.com/maps/api/staticmap?";
+    float span = 5f;
+    private float currentTime = 0f;
+
 
     // Use this for initialization
-    void Start () {
-        Build ();
+    void Start()
+    {
+        MapDisplay(SampleScript.mylat, SampleScript.mylon);
     }
     
     // Update is called once per frame
-    void Update () {
-        
+    void Update()
+    {
+        // 位置情報を送受信
+        currentTime += Time.deltaTime;
+        if(currentTime > span)
+        {
+            Debug.Log("map");
+            currentTime = 0f;
+            MapDisplay(SampleScript.mylat, SampleScript.mylon);
+        }
     }
 
-    public void Build(){
+    public void MapDisplay(float lat, float lon)
+    {
+        string Url = @"https://maps.googleapis.com/maps/api/staticmap?";
 
         // 中心座標 
-        Url += "center=" + lat + "," + lng;
+        Url += "center=" + lat + "," + lon;
 
         // ズームレベル
         Url += "&zoom=" + zoom;
@@ -34,24 +45,29 @@ public class DrawGoogleMap : MonoBehaviour {
         // 地図画像のサイズ
         Url += "&size=800x800";
 
-        if (key != null && key.Length != 0) {
+        if (key != null && key.Length != 0)
+        {
             Url += "&key=" + key;
         }
 
         Url = Uri.EscapeUriString(Url);
-        StartCoroutine(Download(this.Url, tex => addSplatPrototype(tex)));
+        StartCoroutine(Download(Url, tex => addSplatPrototype(tex)));
     }
 
-    /// GoogleMapsAPIから地図画像をダウンロードする
-    IEnumerator Download(string url, Action<Texture2D> callback) {
+    // GoogleMaps APIから地図画像をダウンロードする
+    IEnumerator Download(string url, Action<Texture2D> callback)
+    {
         var www = new WWW(url);
+        Debug.Log(url);
+        Debug.Log("header" + www.texture);
         yield return www; // Wait for download to complete
 
         callback(www.texture);
     }
 
-    /// imageにテクスチャを貼り付ける
-    public void addSplatPrototype(Texture2D tex) {
+    // imageにテクスチャを貼り付ける
+    public void addSplatPrototype(Texture2D tex)
+    {
         GetComponent<Image> ().sprite = Sprite.Create(tex, new Rect(0,0,tex.width,tex.height), Vector2.zero);
     }
 }
